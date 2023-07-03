9802a5afec1ade1b8e5bb60e15701910df83b8b5
Jonas <jonasdalethese1@gmail.com>
2023-07-03 11:17:23 -0300

    overflow hidden no signUp

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
   max-width: 375px;
   height: 100vh;
   background: ${colors.white};
-  overflow: scroll;
+  overflow: hidden;
 
   picture img {
     width: 100%;
